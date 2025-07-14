ef71a3aabba34221ccebef392ea5a256a9cbe1b6
JhonMG07 <jmeza050@gmail.com>
2025-07-13 21:49:42 -0500

    Refactor docker-compose.yml to comment out database service and update database.py to configure SSL for async engine connection

--- app/database.py
+++ app/database.py
@@ -1,10 +1,16 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
+import ssl
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+ssl_context = ssl.create_default_context()
 
-engine = create_async_engine(DATABASE_URL, echo=True)
+engine = create_async_engine(
+    DATABASE_URL.replace("?sslmode=require", ""),  # limpia la URL
+    echo=True,
+    connect_args={"ssl": ssl_context}
+)
 SessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
 Base = declarative_base()
 
